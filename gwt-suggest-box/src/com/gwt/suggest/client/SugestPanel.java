065e139aa23e4a30bebfc394357cff559834d591
viniciuscunegatto@gmail.com <viniciuscunegatto@gmail.com@0dfeecf5-a28b-53b4-1cac-e97bc600ec2e>
2014-02-17 19:18:00 +0000

    criacao dos menus

--- gwt-suggest-box/src/com/gwt/suggest/client/SugestPanel.java
+++ gwt-suggest-box/src/com/gwt/suggest/client/SugestPanel.java
@@ -14,116 +14,137 @@ import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 
-public class SugestPanel extends AbsolutePanel{
-	
-	Label nome = new Label();
+public class SugestPanel extends AbsolutePanel {
+
+	Label name = new Label();
 	Label email = new Label();
-	Label data = new Label();
-	Label assunto = new Label();
-	Label setor = new Label();
-	Label sugestao = new Label();
-	TextBox nomeT = new TextBox();
+	Label date = new Label();
+	Label subject = new Label();
+	Label sector = new Label();
+	Label suggest = new Label();
+	TextBox nameT = new TextBox();
 	TextBox emailT = new TextBox();
-	TextBox dataT = new TextBox();
-	TextBox assuntoT = new TextBox();
-	ListBox setorL = new ListBox();
-	TextArea sugestaoT = new TextArea();
-	Button limpar = new Button();
-	Button cancelar = new Button();
-	Button enviar = new Button();
+	TextBox dateT = new TextBox();
+	TextBox subjectT = new TextBox();
+	ListBox sectorL = new ListBox();
+	TextArea suggestT = new TextArea();
+	Button clean = new Button();
+	Button cancel = new Button();
+	Button send = new Button();
 	AbsolutePanel p = new AbsolutePanel();
 
-	
-	public SugestPanel(){
-		
-		//sets labels
-		nome.setText("Nome: ");
+	public SugestPanel() {
+
+		// sets labels
+		name.setText("Nome: ");
 		email.setText("Email: ");
-		data.setText("Data: ");
-		assunto.setText("Assunto: ");
-		setor.setText("Setor: ");
-		sugestao.setText("Sugestao: ");
-		limpar.setText("Limpar");
-		cancelar.setText("Cancelar");
-		enviar.setText("Enviar");
-		
-		//set
-		assuntoT.setWidth("530px");
-		dataT.setText(getDate());
-		dataT.setEnabled(false);
-		setorL.addItem("---");
-		setorL.addItem("RH");
-		setorL.addItem("Infra");
-		setorL.addItem("Higiene");
-		setorL.addItem("Diversos");
-		sugestaoT.setSize("600px", "250px");
+		date.setText("Data: ");
+		subject.setText("Assunto: ");
+		sector.setText("Setor: ");
+		suggest.setText("Sugestao: ");
+		clean.setText("Limpar");
+		cancel.setText("Cancelar");
+		send.setText("Enviar");
+
+		// set
+		subjectT.setWidth("530px");
+		dateT.setText(getDate());
+		dateT.setEnabled(false);
+		sectorL.addItem("---");
+		sectorL.addItem("RH");
+		sectorL.addItem("Infra");
+		sectorL.addItem("Higiene");
+		sectorL.addItem("Diversos");
+		suggestT.setSize("600px", "250px");
 		this.setSize("900px", "500px");
-		
-		//click listener
-		limpar.addClickHandler(new ClickHandler() {
+
+		// click listener
+		clean.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				if(Window.confirm("Tem certeza que deseja limpar?")){
+				if (Window.confirm("Tem certeza que deseja limpar?")) {
 					limpar();
-					nomeT.setFocus(true);
-				}else{
-					
+					nameT.setFocus(true);
+				} else {
+
 				}
-				
-								
+
 			}
 
 		});
-		
-		cancelar.addClickHandler(new ClickHandler() {
+
+		cancel.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				if(Window.confirm("Tem certeza que deseja sair?")){
+				if (Window.confirm("Tem certeza que deseja sair?")) {
 					Main.midPanel.clear();
 					MenuEsquerda.habilitaMenu(true);
-				}else{
-					sugestaoT.setFocus(true);
+				} else {
+					suggestT.setFocus(true);
 				}
-				
-								
+
 			}
 
 		});
 		
-		
-		
-		this.add(nome,10,14);
-		this.add(nomeT,70,10);
-		this.add(email,10,54);
-		this.add(emailT,70,50);
-		this.add(data,10,94);
-		this.add(dataT,70,90);
-		this.add(setor,260,94);
-		this.add(setorL,310,93);
-		this.add(assunto,10,134);
-		this.add(assuntoT,70,130);
-		this.add(sugestao,10,170);
-		this.add(sugestaoT,10,190);
-		this.add(limpar,10,460);
-		this.add(cancelar,490,460);
-		this.add(enviar,570,460);
-		
-		
-		
+		send.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				
+				if (sectorL.getSelectedIndex()==0){
+					Window.alert("O campo Setor deve ser selecionado corretamente");
+					sectorL.setFocus(true);				
+					
+				} else if (subjectT.getText().equals("")) {
+					Window.alert("O campo Assunto deve ser preenchido");
+					subjectT.setFocus(true);
+					
+				}else if (suggestT.getText().equals("")){
+					Window.alert("O campo Sugestao deve ser preenchido");
+					suggestT.setFocus(true);
+					
+				}else {
+					if(Window.confirm("Tem certeza que deseja enviar?")){
+						System.out.println("Enviado");
+						limpar();
+					}
+				}
+
+			}
+
+			
+
+		});
+
+		this.add(name, 10, 14);
+		this.add(nameT, 70, 10);
+		this.add(email, 10, 54);
+		this.add(emailT, 70, 50);
+		this.add(date, 10, 94);
+		this.add(dateT, 70, 90);
+		this.add(sector, 260, 94);
+		this.add(sectorL, 310, 93);
+		this.add(subject, 10, 134);
+		this.add(subjectT, 70, 130);
+		this.add(suggest, 10, 170);
+		this.add(suggestT, 10, 190);
+		this.add(clean, 10, 460);
+		this.add(cancel, 490, 460);
+		this.add(send, 570, 460);
+
 	}
-	
-	public static String getDate(){
-		String data;
+
+	public static String getDate() {
+		String dateS;
 		Date date = new Date();
 		DateTimeFormat dtf = DateTimeFormat.getFormat("dd/MM/yyyy - HH:mm:ss");
-		data=(dtf.format(date, TimeZone.createTimeZone(180)));
-		return data;
+		dateS = (dtf.format(date, TimeZone.createTimeZone(180)));
+		return dateS;
 	}
-	
-	private void limpar(){
-		nomeT.setText("");
+
+	private void limpar() {
+		nameT.setText("");
 		emailT.setText("");
-		assuntoT.setText("");
-		setorL.setSelectedIndex(0);
-		sugestaoT.setText("");
+		subjectT.setText("");
+		sectorL.setSelectedIndex(0);
+		suggestT.setText("");
 	}
 
 }
